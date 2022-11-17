e1233edf9999c5534985064430186a3980d4277b
OpenSourcePundit <shashank00098@gmail.com>
2022-11-18 05:25:44 +0530

    btn-translate is incorporated

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-console.log("Hey, script is working from another file, app.js")
+/*console.log("Hey, script is working from another file, app.js")
 
 var username = prompt("Give me username")
 var welcomeMssage="This script works 2 ways "+username;
-alert(welcomeMssage)
+alert(welcomeMssage)*/
+
+var btnTranslate = document.querySelector("#btn-translate");
+console.log(btnTranslate);
