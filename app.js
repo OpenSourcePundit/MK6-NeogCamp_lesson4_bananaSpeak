e37df3e9542c95df2f5467eca0d7ef4b2abbb285
OpenSourcePundit <shashank00098@gmail.com>
2022-11-18 05:36:05 +0530

    Dom_scripting example in Console

--- app.js
+++ app.js
@@ -6,3 +6,5 @@ alert(welcomeMssage)*/
 
 var btnTranslate = document.querySelector("#btn-translate");
 console.log(btnTranslate);
+//once correctly reffered  it can be saved as global variable. then can be used to make changes in html through console itself_eg-
+// temp1.style.backgroundColor="red"; ENTER turns button to red______ DOM_SCRIPTING
