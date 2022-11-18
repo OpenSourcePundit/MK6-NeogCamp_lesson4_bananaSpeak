e43aaa7ae2315fb538dbeef49a814c26a1185e70
OpenSourcePundit <shashank00098@gmail.com>
2022-11-18 15:20:49 +0530

    Query selector ex and dom scripting
    Here in browser console several things are done to tell about query selector, document.querySelector("xxx") actully searched for the id xxx in document.

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ btnTranslate.addEventListener("click",function clickEventHandler(){
     x++;
     console.log("clicked "+x+" times")
 })
+
+/*
+checking QUERYSELECTOR
+ in browser console -- wrote  
+ document                                            this gives the complete document also can write :
+ document.body                                       this gives complete body attributes
+ document.body.style.backgroundColor="green"         to ged background green
+ */
+
