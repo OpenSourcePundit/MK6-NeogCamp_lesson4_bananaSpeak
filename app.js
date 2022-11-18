01027f0cc2f29e4575422f1653d309dea72896c6
OpenSourcePundit <shashank00098@gmail.com>
2022-11-19 02:55:31 +0530

    Output modelling done
    Output div made and js attached. also important usage of innerText is done . '.value' and '.innertext' are quite similar.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var x=0;
 var txtinput = document.querySelector("#txtinput");
+var outputDiv = document.querySelector("#output");
+btnTranslate.addEventListener("click",clickHandler);
+
 function clickHandler() {
-    x++;
-    console.log("clicked " + x + " times from external function");
-    console.log(" hey thats it ",txtinput.value);
+   outputDiv.innerText="asasjsjsjs "+ txtinput.value;
 }
-/*console.log("hey thats it"+text1)   We can also read this value by inspect and the store it as global variable(textarea), the use "variable.value"*/
-    btnTranslate.addEventListener("click",clickHandler);
+
+    
