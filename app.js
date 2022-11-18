ce599ee2e5d172a08034c92a553c85068ff5c15a
OpenSourcePundit <shashank00098@gmail.com>
2022-11-18 05:49:26 +0530

    addEventListner added

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-/*console.log("Hey, script is working from another file, app.js")
-
-var username = prompt("Give me username")
-var welcomeMssage="This script works 2 ways "+username;
-alert(welcomeMssage)*/
-
 var btnTranslate = document.querySelector("#btn-translate");
-console.log(btnTranslate);
-//once correctly reffered  it can be saved as global variable. then can be used to make changes in html through console itself_eg-
-// temp1.style.backgroundColor="red"; ENTER turns button to red______ DOM_SCRIPTING
+
+btnTranslate.addEventListener("click",function clickEventHandler(){
+    console.log("clicked !")
+})
