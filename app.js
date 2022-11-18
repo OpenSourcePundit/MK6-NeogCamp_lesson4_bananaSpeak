c8624e6f184d4a852de5b78e0001445e4c7422d4
OpenSourcePundit <shashank00098@gmail.com>
2022-11-18 23:18:35 +0530

    Some updates as function was not working
    The function definition was not working, So removed function definition (clickHandler) and again the script is working

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var x=0;
 var txtinput = document.querySelector("#txtinput");
-/*console.log("hey thats it"+text1);//We can also read this value by inspect and the store it as global variable(textarea), the use "variable.value"*/
-btnTranslate.addEventListener("click",clickHandler());
-
-
-function clickHandler() {
+/*console.log("hey thats it"+text1)   We can also read this value by inspect and the store it as global variable(textarea), the use "variable.value"*/
+    btnTranslate.addEventListener("click",function clickHandler() {
         x++;
         console.log("clicked " + x + " times from external function");
         console.log(" hey thats it ",txtinput.value);
-    };
-
+    });
