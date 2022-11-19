9e7691ae82a38a41ee2114a1258b045706a1397e
OpenSourcePundit <shashank00098@gmail.com>
2022-11-19 15:40:07 +0530

    Final updations along with errorHandler
    errorhandler() used to tackle errors, .catch is used for error handling. Also final serverURL made live and APP is in working conditions

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtinput = document.querySelector("#txtinput");
 var outputDiv = document.querySelector("#output");
-var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-btnTranslate.addEventListener("click",clickHandler);
-function getTranslationURL(text){
+
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+//var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslationURL(text){    //get mdified GET url including text
     return serverURL+"?text="+text;
 }
-function clickHandler() {    
-    var inputText = txtinput.value;
-   fetch(getTranslationURL(inputText))
-   .then(fuck => fuck.json())
-   .then(fux1 => console.log(fux1.contents.translated))
+
+function errorHandler(error){ //errorhandling using catch , this is a callback function
+    console.log("Error Occured"+error);
+    alert("Error Occured, Please try after sometime")
 }
+
+function clickHandler() {    
+    var inputText = txtinput.value; //extracting inputText
+    //fetch using translation URL, calling server for processing
+   fetch(getTranslationURL(inputText)) 
+   .then(fuck => fuck.json())
+   .then(fux1 => {var translatedText=fux1.contents.translated;
+    outputDiv.innerText=translatedText;}) //output as innertext
+    .catch(errorHandler)
+   }
+
+   btnTranslate.addEventListener("click",clickHandler);
